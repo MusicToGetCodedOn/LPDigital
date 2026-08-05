78773d224b949fdb84c3204f07691873b77fd05d
Loris Perez <lpe149399@stud.gibb.ch>
2026-08-05 10:55:45 +0200

    db stuff

--- backend/prisma/migrations/20260721210625_add_documents/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Document" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "fileUrl" TEXT NOT NULL,
-    "fileSize" TEXT NOT NULL,
-    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
