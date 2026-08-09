e94e985cc215a2a0ef4859d74ce74ad3f0c132f5
Loris Perez <lpe149399@stud.gibb.ch>
2026-08-09 20:48:54 +0200

    changed allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ const allowedOrigins = [
   "http://localhost",
   "http://192.168.1.130",
   "http://192.168.1.130:80",
+  'https://lpdigital.ch',
+  'https://www.lpdigital.ch',
   clientUrl,
 ].filter(Boolean);
 
