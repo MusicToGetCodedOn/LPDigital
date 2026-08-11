dfb2bcfa38898b5f6f888e079fb18671398ed931
Loris Perez <lpe149399@stud.gibb.ch>
2026-08-11 19:58:06 +0200

    more helmet stuff

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,7 @@ app.use(express.json());
 
 app.use(helmet({
   crossOriginResourcePolicy: {policy: "cross-origin"},
+  crossOriginEmbedderPolicy: false, 
 }
 ));
 app.use(express.json({ limit: "10kb" }));
