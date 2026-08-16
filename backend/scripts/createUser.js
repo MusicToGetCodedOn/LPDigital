7efe73ac3585e43d5b2e0f8ab5b2b8aa6c696b87
Loris Perez <lpe149399@stud.gibb.ch>
2026-08-16 14:47:51 +0200

    changed bcrypt import

--- backend/scripts/createUser.js
+++ backend/scripts/createUser.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
